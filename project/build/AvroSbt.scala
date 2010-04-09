82424e32e2432d31026408bf790a7ad8651355c6
Coda Hale <coda.hale@gmail.com>
2010-04-08 18:06:22 -0700

    Push to my repo.

--- project/build/AvroSbt.scala
+++ project/build/AvroSbt.scala
@@ -1,4 +1,16 @@
 class AvroSbt(info: sbt.ProjectInfo) extends sbt.PluginProject(info) {
   val avro = "org.apache.hadoop" % "avro" % "1.3.2" withSources()
   val jopt = "net.sf.jopt-simple" % "jopt-simple" % "3.2" withSources()
+  
+  /**
+   * Publish to a local temp repo, then rsync the files over to repo.codahale.com.
+   */
+  override def managedStyle = sbt.ManagedStyle.Maven
+  val publishTo = sbt.Resolver.file("Local Cache", ("." / "target" / "repo").asFile)
+  def publishToLocalRepoAction = super.publishAction
+  override def publishAction = task {
+    log.info("Uploading to repo.codahale.com")
+    sbt.Process("rsync", "-avz" :: "target/repo/" :: "codahale.com:/home/codahale/repo.codahale.com" :: Nil) ! log
+    None
+  } describedAs("Publish binary and source JARs to repo.codahale.com") dependsOn(test, publishToLocalRepoAction)
 }
